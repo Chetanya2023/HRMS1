b3769cb41560f39b6e911305c3e3567c9877e796
Chetanya <chetanyapost4u@gmail.com>
2023-05-04 18:47:39 +0530

    first commit HRMS

--- src/test/java/CProject/hrms.java
+++ src/test/java/CProject/hrms.java
@@ -32,14 +32,15 @@ public class hrms {
 		//login validations and Flow
 		WebElement valid1 = driver.findElement(By.xpath("(//button[@type='submit'])[1]"));
 		valid1.click();
-		
+		//Thread.sleep(2000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
 		WebElement emailvalidation = driver.findElement(By.id("login_email_help"));
 		String validation = emailvalidation.getText();
 		
 		WebElement passvalidation = driver.findElement(By.id("login_password_help"));
 		String passwordvalidation = passvalidation.getText();
 		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
-		Thread.sleep(2000);
+		
 		if(validation.contains("Please enter your email"))
 				{
 			WebElement login = driver.findElement(By.id("login_email"));
@@ -109,13 +110,31 @@ public class hrms {
 		}
 		
 		//Employee-Department
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
 		driver.findElement(By.xpath("(//div[@class='ant-form-item-control-input-content'])[3]")).click();
 		driver.findElement(By.xpath("//div[text()='QA']")).click();
 		driver.findElement(By.id("first_name")).sendKeys("Rupal");
 		driver.findElement(By.id("last_name")).sendKeys("Shah");
 		driver.findElement(By.id("phoneNo")).sendKeys("9871234560");
 		driver.findElement(By.id("alternateNo")).sendKeys("9874561230");
-		driver.findElement(By.id("aadharcardNo")).sendKeys("1234567890");
+		driver.findElement(By.id("aadharcardNo")).sendKeys("123456789012");
+		driver.findElement(By.id("age")).sendKeys("25");
+		driver.findElement(By.id("joiningdate")).sendKeys("12-12-2003");
+		driver.findElement(By.id("releavingDate")).sendKeys("12-04-2023");
+		driver.findElement(By.id("gender")).click();
+		driver.findElement(By.xpath("//div[text()='Female']")).click();
+		driver.findElement(By.id("salary")).sendKeys("25000");
+		driver.findElement(By.id("email")).sendKeys("rupal@mailinator.com");
+		driver.findElement(By.id("personalemailId")).sendKeys("rupal1@mailinator.com");
+		driver.findElement(By.id("password")).sendKeys("Test@1234");
+		driver.findElement(By.id("permanent_address")).sendKeys("Gr Circle , New Delhi , 385555");
+		driver.findElement(By.id("temparory_address")).sendKeys("NA");
+		driver.findElement(By.id("birthdate")).sendKeys("22-12-2000");
+		driver.findElement(By.id("city")).sendKeys("Delhi");
+		driver.findElement(By.xpath("//span[text()='OK']")).click();
+		
+		
+		
 		
 		
 		
